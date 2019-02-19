4523881e77ca501dd5a407eb833d7b93ff23a705
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-19 00:13:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num == 2 || num == 3
+    return true
+  end
+  for i in 2..(num/2)
+    if num%i == 0
+      return false
+    end
+  end
+  true
+end
+
+# for d in 2..(n - 1)
+#   if (n % d) == 0
+#     return false
+#   end
+#   end
+
+
+prime?(1763)
