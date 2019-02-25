e4afceb54f25aa15e7866e262aa8ffb2ecac695a
Samuel Guo <guosamuel1114@gmail.com>
2019-02-25 23:32:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(integer)
+  
+  if integer < 0
+    return false
+  elsif integer == 1
+    return false
+  elsif integer == 0
+    return false
+  end 
+  
+  [*2..integer-1].none? { |divisor| integer % divisor == 0}
+  
+end
