06db31117afbc9ae2730b8cc1f899c412d7eafe8
John Lee <jslee1992@gmail.com>
2019-03-07 12:13:41 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number == 2
+    true
+  elsif number <= 1
+    false
+  elsif (2...number).all? {|x| number % x != 0}
+    true
+  else
+    false
+  end
+end
